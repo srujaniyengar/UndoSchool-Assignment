cff7065bfb6a9f89b6ba445dab7046186e358c0f
srujaniyengar <srujanparthasarathyiyengar@gmail.com>
2025-07-16 20:02:06 +0530

    add : 30 more courses

--- src/main/java/com/app/backend/document/CourseDocument.java
+++ src/main/java/com/app/backend/document/CourseDocument.java
@@ -5,13 +5,16 @@ import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
 
 import java.time.Instant;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Document(indexName = "courses") // This will use the 'courses' index in Elasticsearch
+@Document(indexName = "courses")
 public class CourseDocument {
 
     @Id
@@ -20,12 +23,13 @@ public class CourseDocument {
     private String title;
     private String description;
     private String category;
-    private String type; // e.g., "online", "offline"
+    private String type;
     private int minAge;
     private int maxAge;
     private double price;
+
+    @Field(type = FieldType.Date, format = DateFormat.date_time)
     private Instant nextSessionDate;
 
-    // Add other fields as needed
-
+    private String gradeRange;
 }
